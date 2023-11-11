7c97e66dd6f0b55213578ea14edf36068468df11
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:53:01 +0000

    Update

---
+++ src/ChattyP2P_Cli/Program.py
@@ -0,0 +1,8 @@
+from Logging import *
+from _global import *
+from TCPClient import *
+
+LOGGINGINIT()
+TCPCLIENTMAIN()
+
+
--- src/ChattyP2P_Srv/TCPServer.py
+++ src/ChattyP2P_Srv/TCPServer.py
@@ -1,32 +1,31 @@
 import socket
-from MessageHanding import *
 from Logging import *
 from _global import *
-from MessageHanding import *
+from MessageHandling import *
 import os
 
 def TCPSRVMAIN():
     
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    try:
-        server.bind((IPADDRESS, PORT))
-        LOGEVENTS_DEBUG("Bound to socket")
-    except Exception as ex:
-        LOGEVENTS_ERROR(f"{ex}")
-        LOGEVENTS_CRITICAL("Terminating CHATTY_SRV_PY")
-        quit(code=1)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: 
+        try:
+            server.bind((IPADDRESS, PORT))
+            LOGEVENTS_DEBUG("Bound to socket")
+        except Exception as ex:
+            LOGEVENTS_ERROR(f"{ex}")
+            LOGEVENTS_CRITICAL("Terminating CHATTY_SRV_PY")
+            quit(code=1)
 
-    server.listen(1)
-    LOGEVENTS_DEBUG(f"Server listening on {PORT}")
+        server.listen(1)
+        LOGEVENTS_DEBUG(f"Server listening on {PORT}")
 
-    LOGEVENTS_INFO("Waiting for client connection")
+        LOGEVENTS_INFO("Waiting for client connection")
 
-    client, address = server.accept()
-    LOGEVENTS_DEBUG(f"Client connected to {address}")
+        client, address = server.accept()
+        LOGEVENTS_DEBUG(f"Client connected to {address}")
 
-    while True:
-        print("Enter a message:")
-        MSGHANDLER(input(), client)
+        while True:
+            print("Enter a message:")
+            MSGHANDLER(input(), client)
 
         
 
