db633ca081fef0ca84efb6397327939cf88a0463
Peter Hamilton <peterhamilton522@Gmail.com>
2023-11-11 19:53:01 +0000

    Fixed some errors

--- src/ChattyP2P_Cli/Program.py
+++ src/ChattyP2P_Cli/Program.py
@@ -1,6 +1,8 @@
 from Logging import *
 from _global import *
 from TCPClient import *
+import threading
+
 
 LOGGINGINIT()
 TCPCLIENTMAIN()
--- src/ChattyP2P_Cli/TCPClient.py
+++ src/ChattyP2P_Cli/TCPClient.py
@@ -2,12 +2,22 @@ import socket
 from MessageHandling import *
 from Logging import *
 from _global import *
+import threading
 
 def TCPCLIENTMAIN():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((IP, PORT))
+        try:
+            client.connect((IP, PORT))
+            LOGEVENTS_DEBUG("Connecting to server")
+        except Exception as ex:
+            LOGEVENTS_ERROR(f"{ex}")
+            LOGEVENTS_CRITICAL("Terminating CHATTYP2P_Cli")
+            quit(code=1)
+
+        
+        threading.Thread(target=MSGRECV(client)).start()
+        print("Enter a message:")
         while True:
-            print("Enter a message:")
             MSGHANDLER(input(), client)
 
 
